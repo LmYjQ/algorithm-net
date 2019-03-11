f7087f3b3d5674aab0d709d0d1987dbda99e8f91
a123 <qiyu@ibangtang.com>
2019-03-11 20:13:35 +0800

    prepare

---
+++ script/prepare_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import argparse
+import sys
+
+sheet_names = []
+inpath = 
+out_folder = 
+
+def main():
+   # inpath = sys.argv[1]
+   # out_folder = sys.argv[2]
+    df = pd.read_excel(inpath)
+    
+
+
+if __name__=='__main__':
+    main()
+
