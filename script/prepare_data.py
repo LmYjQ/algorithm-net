0d22367a3b6dc09e2f557590252ce12be6fe0769
LmYjQ <liu1quan@qq.com>
2019-03-14 22:31:31 +0800

    sheet name conf

--- script/prepare_data.py
+++ script/prepare_data.py
@@ -1,33 +1,32 @@
 import pandas as pd
 import argparse
 import os,sys
+from config import * 
 
-sheet_names = []
-inpath =  '/Users/maerkesu/Downloads/'
-neo4j_path = '/Users/maerkesu/Downloads/neo4j-community-3.5.1' 
-
-inpath = '/Users/a123/Downloads/'
-neo4j_path = '/Users/a123/Downloads/neo4j-community-3.4.1'
-
-def cmd_proc():
+def cmd_proc(cmd):
     print(cmd)
     os.system(cmd)
 
 def stop_neo4j():
     cmd = "{}  stop".format(os.path.join(neo4j_path,'bin','neo4j'))
     cmd_proc(cmd)
-    cmd = "rm {}".format(os.path.join(neo4j_path,'import','*')
+    cmd = "rm {}".format(os.path.join(neo4j_path,'import','*'))
     cmd_proc(cmd)
 
-def format_raw_file(conf):
-    df = pd.read_excel(path)
+def format_raw_file():
+    print(conf.node.sheet_names,conf.edge.sheet_names)
+    nodes = [(conf.node.file_name,i) for i in conf.node.sheet_names.split(',')]
+    edges = [(conf.edge.file_name,i) for i in conf.edge.sheet_names.split(',')]
+    print(nodes,edges)
+    #df = pd.read_excel(conf.path.inpath)
 
 
 def main():
-   # inpath = sys.argv[1]
-   # out_folder = sys.argv[2]
     conf_file = sys.argv[1]
-    
+    global conf
+    conf = parse_config(conf_file)
+    print(conf)
+    format_raw_file()
     
 
 
