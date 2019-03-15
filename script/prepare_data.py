c8808e55d70cfc8825b6838c5f9c202a1e1efd8c
LmYjQ <liu1quan@qq.com>
2019-03-15 08:00:02 +0800

    0315

--- script/prepare_data.py
+++ script/prepare_data.py
@@ -13,19 +13,27 @@ def stop_neo4j():
     cmd = "rm {}".format(os.path.join(neo4j_path,'import','*'))
     cmd_proc(cmd)
 
+def start_neo4j():
+    pass
+
 def format_raw_file():
     print(conf.node.sheet_names,conf.edge.sheet_names)
     nodes = [(conf.node.file_name,i) for i in conf.node.sheet_names.split(',')]
     edges = [(conf.edge.file_name,i) for i in conf.edge.sheet_names.split(',')]
-    print(nodes,edges)
-    #df = pd.read_excel(conf.path.inpath)
+    for tup in nodes+edges:
+        in_path = os.path.join(conf.path.inpath,tup[0])
+        print(in_path)
+        df = pd.read_excel(in_path,sheet_name=tup[1])
+        out_path = os.path.join(conf.path.neo4j_path,'import',tup[1])
+        print(out_path)
+        df.to_csv(out_path,index=False,sep='\t')
+
 
 
 def main():
     conf_file = sys.argv[1]
     global conf
     conf = parse_config(conf_file)
-    print(conf)
     format_raw_file()
     
 
