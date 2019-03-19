2c9c798c51a127cf592ae6dc040e96d5b2635e1d
a123 <qiyu@ibangtang.com>
2019-03-19 19:25:56 +0800

    create node and relationships

--- script/prepare_data.py
+++ script/prepare_data.py
@@ -10,7 +10,7 @@ def cmd_proc(cmd):
 def stop_neo4j():
     cmd = "{}  stop".format(os.path.join(conf.path.neo4j_path,'bin','neo4j'))
     cmd_proc(cmd)
-    cmd = "rm -r {}".format(os.path.join(conf.path.neo4j_path,'data','databases','graph.db'))
+    cmd = "rm -r {}".format(os.path.join(conf.path.neo4j_path,'data','databases','*.db'))
     cmd_proc(cmd)
 
 def start_neo4j():
@@ -20,18 +20,17 @@ def start_neo4j():
 
 def format_raw_file():
     print(conf.node.sheet_names,conf.edge.sheet_names)
-    #nodes = [(conf.node.file_name,i,'node') for i in conf.node.sheet_names.split(',')]
-    #edges = [(conf.edge.file_name,i,'edge',conf.edge.link_objs) for i in conf.edge.sheet_names.split(',')]
     for conf_obj in [conf.node,conf.edge]: # conf = global conf; conf_obj = conf for node OR edge 
         for _type in conf_obj.sheet_names.split(','): 
-            build_cypher(conf_obj,_type)
+            cypher = build_cypher(conf_obj,_type)
+            print(cypher)
 
 def split_save(conf_obj,sheet_name):
     in_path = os.path.join(conf.path.inpath,conf_obj.file_name)
     #print(in_path)
     df = pd.read_excel(in_path,sheet_name=sheet_name)
     out_path = os.path.join(conf.path.neo4j_path,'import',sheet_name)
-    print(out_path)
+    #print(out_path)
     df.to_csv(out_path,index=False,sep='\t')
     return df
   
@@ -42,11 +41,12 @@ def build_cypher(conf_obj,_type):
     if conf_obj.obj_type=='node':
         path = 'file:///{}'.format(sheet_name)
         nodes_def = ','.join(['{}:row.{}'.format(e,e) for e in cols])
-        create = 'CREATE (:%s { %s });'%(sheet_name,nodes_def)
+        create = 'CREATE (:%s { %s })'%(sheet_name,nodes_def)
         return '''
                USING PERIODIC COMMIT
                LOAD CSV WITH HEADERS FROM '{}' AS row
-               {} '''.format(path, create)
+               FIELDTERMINATOR '\\t'
+               {}; '''.format(path, create)
     else:
         path = 'file:///{}'.format(sheet_name)
         #_type = extend:Model#model-EXTEND_TO-ModelExtend#model
@@ -55,26 +55,31 @@ def build_cypher(conf_obj,_type):
         [node1_name,node2_name] = [e.split('#')[0] for e in [n1,n2]]
         [node1_type,node2_type] = [e.split('#')[1] for e in [n1,n2]]
         #[node_df1,node_df2] = [pd.read_csv(e,sep='\t') for e in [node1_name,node2_name]]
-        node1 = '(%s:%s {%s: row.from_id})' % (node1_name,node1_type,node1_type+'ID')
-        node2 = '(%s:%s {%s: row.to_id})' % (node2_name,node2_type,node2_type+'ID')
-        link = '(%s)-[r:%s]->(%s) ON CREATE SET r.how=row.property' %s (node1_name,r,node2_name)
+        match = '(a:%s {name: row.from_id}),(b:%s {name: row.to_id})' % (node1_type,node2_type)
+        create = '(a)-[:%s {how: row.property} ]->(b)' % (r)
+       # node1 = '(%s:%s {%s: row.from_id})' % (node1_name,node1_type,'name')
+       # node2 = '(%s:%s {%s: row.to_id})' % (node2_name,node2_type,'name')
+       # link = '(%s)-[r:%s]->(%s) ON CREATE SET r.how=row.property;' % (node1_name,r,node2_name)
         return '''
                USING PERIODIC COMMIT
                LOAD CSV WITH HEADERS FROM '{}' AS row
+               FIELDTERMINATOR '\\t'
                MATCH {}
-               MATCH {}
-               MERGE {} '''.format(path,node1,node2,link)
+               CREATE {} ;'''.format(path,match,create)
  
-
-
-
 def main():
-    conf_file = sys.argv[1]
+    parser = argparse.ArgumentParser(description="bravo!")
+    parser.add_argument('--conf_file','-c')
+    parser.add_argument('--restart','-r')
+    args = parser.parse_args() 
+    conf_file = args.conf_file
+    restart = args.restart 
     global conf
     conf = parse_config(conf_file)
-#    stop_neo4j()
+    if restart:
+        stop_neo4j()
+        start_neo4j()
     format_raw_file()
-#    start_neo4j()
     
 
 
